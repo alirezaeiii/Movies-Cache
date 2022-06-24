9d93d905e2038e2378cee7f34f83cd5cdadec713
Ali Rezaei <ali.rezaei09@gmail.com>
2022-06-25 00:10:15 +0430

    Cleanup

--- app/src/main/java/com/zattoo/movies/utils/DataUtils.kt
+++ app/src/main/java/com/zattoo/movies/utils/DataUtils.kt
@@ -2,20 +2,10 @@ package com.zattoo.movies.utils
 
 import com.zattoo.movies.data.MovieListEntity
 import com.zattoo.movies.data.MovieListOffers
-import com.zattoo.movies.data.MovieService
 import com.zattoo.movies.data.home.Currency
 import com.zattoo.movies.data.home.Image
 import com.zattoo.movies.data.home.Movie
 import com.zattoo.movies.data.home.Price
-import kotlinx.coroutines.runBlocking
-
-fun fetchMovieList(service: MovieService) = runBlocking {
-    service.fetchMovieList()
-}
-
-fun fetchMovieListOffers(service: MovieService) = runBlocking {
-    service.fetchMovieListOffers()
-}
 
 fun createMovies(
     movieDetails: List<MovieListEntity.MovieData>,
