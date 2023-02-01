fb82fd457733a70150d8f721cf6beccae6943a9a
Ali <ali.rezaei09@gmail.com>
2023-02-01 14:08:49 +0330

    Modification

--- app/src/main/java/com/sample/movies/ui/MainActivity.kt
+++ app/src/main/java/com/sample/movies/ui/MainActivity.kt
@@ -30,15 +30,11 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment)
         val appBarConfiguration = AppBarConfiguration(navController.graph)
         binding.toolbar.setupWithNavController(navController, appBarConfiguration)
-    }
-
-    override fun onResume() {
-        super.onResume()
         handleNetwork()
     }
 
-    override fun onPause() {
-        super.onPause()
+    override fun onDestroy() {
+        super.onDestroy()
         networkUtils.unRegister()
     }
 
